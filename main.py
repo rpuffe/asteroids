96588d9a0c74dd3bdd4f495ec7f2fa596f5b8db7
Robert Puffe <49496870+RobertPuffeEldermark@users.noreply.github.com>
2024-10-28 16:21:14 -0500

    Got the triangle to show up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -7,16 +8,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
+    x_center = SCREEN_WIDTH / 2
+    y_center = SCREEN_HEIGHT / 2
+    player = Player(x_center, y_center)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
 
