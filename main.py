0364afd92cc1946336b75d6f0f8f036a231dfaa5
Robert Puffe <49496870+RobertPuffeEldermark@users.noreply.github.com>
2024-10-28 15:45:55 -0500

    added tick rate

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
